b6ee738e6f6e7a597f85a07361e64e4023e2f1fa
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2023-05-10 23:18:13 +0200

    Update build_snapshots.sh

--- src/scripts/build_snapshots.sh
+++ src/scripts/build_snapshots.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 CWD=`pwd`
-$DOCKER="docker run -t --mount type=bind,source=${CWD}/snapshots,target=/snapshots"
+DOCKER="docker run -t --mount type=bind,source=${CWD}/snapshots,target=/snapshots"
 
 if [ ! -d "${CWD}/snapshots" ]; then
     mkdir -p ${CWD}/snapshots
