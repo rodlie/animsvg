f583f97d9a817c6ef767d66703977ed7b9be02c4
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2023-05-29 01:06:07 +0200

    Update build_macos.sh

--- src/scripts/build_macos.sh
+++ src/scripts/build_macos.sh
@@ -22,14 +22,25 @@ set -e -x
 
 CWD=`pwd`
 
-SNAP=${SNAP:-1}
-REL=${REL:-0}
-MKJOBS=${MKJOBS:-8}
+SNAP=1 #${SNAP:-0}
+REL=0 #${REL:-0}
+MKJOBS=${MKJOBS:-4}
+
 COMMIT=`git rev-parse --short HEAD`
 VERSION=`cat ${CWD}/CMakeLists.txt | sed '/friction2d VERSION/!d;s/)//' | awk '{print $3}'`
-TIMESTAMP=${TIMESTAMP:-`date +%Y%m%d`}
-OSX=10.14
 
+TIMESTAMP=${TIMESTAMP:-`date +%Y%m%d`}
+YEAR=${YEAR:-`date +%Y`}
+MONTH=${MONTH:-`date +%m`}
+DAY=${DAY:-`date +%d`}
+
+IS_SNAP="OFF"
+if [ "${SNAP}" = 1 ]; then
+    VERSION="${YEAR}.${MONTH}.${DAY}"
+    IS_SNAP="ON"
+fi
+
+OSX=${OSX:-10.13}
 QT=${QT:-"${HOME}/Qt/5.12.12/clang_64"}
 
 QUAZIP=${QUAZIP:-"${CWD}/quazip"}
@@ -47,7 +58,11 @@ if [ -d "build" ]; then
 fi
 mkdir build && cd build
 
-cmake \
+cmake -G Ninja \
+-DSNAPSHOT=${IS_SNAP} \
+-DSNAPSHOT_VERSION_MAJOR=${YEAR} \
+-DSNAPSHOT_VERSION_MINOR=${MONTH} \
+-DSNAPSHOT_VERSION_PATCH=${DAY} \
 -DGIT_COMMIT=${COMMIT} \
 -DCMAKE_OSX_DEPLOYMENT_TARGET=${OSX} \
 -DCMAKE_BUILD_TYPE=Release \
@@ -59,7 +74,7 @@ cmake \
 -DQSCINTILLA_LIBRARIES_DIRS=${QSCINTILLA}/src \
 -DQSCINTILLA_LIBRARIES=${QSCINTILLA_LIB} \
 ..
-make -j${MKJOBS}
+cmake --build .
 
 ${QT}/bin/macdeployqt src/app/friction.app
 mkdir dmg
@@ -69,10 +84,10 @@ VOLNAME="Friction"
 DMG="friction"
 if [ "${REL}" = 1 ]; then
     VOLNAME="${VOLNAME} ${VERSION}"
-    DMG="${DMG}-${VERSION}-macOS"
+    DMG="${DMG}-${VERSION}-macOS${OSX}"
 elif [ "${SNAP}" = 1 ]; then
-    VOLNAME="${VOLNAME} ${VERSION}-${COMMIT}"
-    DMG="${DMG}-${TIMESTAMP}-${VERSION}-${COMMIT}-macOS"
+    VOLNAME="${VOLNAME} ${TIMESTAMP}-${COMMIT}"
+    DMG="${DMG}-${TIMESTAMP}-${COMMIT}-macOS${OSX}"
 fi
 
 hdiutil create -volname "${VOLNAME}" -srcfolder dmg -ov -format UDBZ ${DMG}.dmg
