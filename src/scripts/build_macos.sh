61d9a8c865593092ffea87bf253ec27824ec59d1
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2023-05-29 16:51:12 +0200

    Update build_macos.sh

--- src/scripts/build_macos.sh
+++ src/scripts/build_macos.sh
@@ -43,6 +43,9 @@ fi
 OSX=${OSX:-10.13}
 QT=${QT:-"${HOME}/Qt/5.12.12/clang_64"}
 
+QUAZIP_V="1.4"
+QSCINTILLA_V="2.13.4"
+
 QUAZIP=${QUAZIP:-"${CWD}/quazip"}
 QUAZIP_LIB=${QUAZIP_LIB:-"quazip1-qt5"}
 
@@ -51,6 +54,48 @@ QSCINTILLA_LIB=${QSCINTILLA_LIB:-"qscintilla2_qt5"}
 
 export PATH="/opt/local/bin:${PATH}"
 export PKG_CONFIG_PATH="/opt/local/lib/pkgconfig"
+
+if [ ! -f "${QUAZIP}/build/quazip/lib${QUAZIP_LIB}.dylib" ]; then
+    if [ -d "${QUAZIP}" ]; then
+        rm -rf ${QUAZIP}
+    fi
+    if [ ! -f "${CWD}/quazip.tar.gz" ]; then
+        curl -L -k "https://github.com/stachenov/quazip/archive/refs/tags/v${QUAZIP_V}.tar.gz" --output ${CWD}/quazip.tar.gz
+    fi
+    cd ${CWD}
+    tar xf quazip.tar.gz
+    mv quazip-${QUAZIP_V} ${QUAZIP}
+    cd ${QUAZIP}
+    mkdir build && cd build
+    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${QT} -DCMAKE_OSX_DEPLOYMENT_TARGET=${OSX} .. && make
+fi
+
+if [ ! -f "${QSCINTILLA}/src/lib${QSCINTILLA_LIB}.dylib" ]; then
+    if [ -d "${QSCINTILLA}" ]; then
+        rm -rf ${QSCINTILLA}
+    fi
+    if [ ! -f "${CWD}/qscintilla.tar.gz" ]; then
+        curl -L -k "https://www.riverbankcomputing.com/static/Downloads/QScintilla/${QSCINTILLA_V}/QScintilla_src-${QSCINTILLA_V}.tar.gz" --output ${CWD}/qscintilla.tar.gz
+    fi
+    cd ${CWD}
+    tar xf qscintilla.tar.gz
+    mv QScintilla_src-${QSCINTILLA_V} ${QSCINTILLA}
+    cd ${QSCINTILLA}/src
+    ${QT}/bin/qmake QMAKE_MACOSX_DEPLOYMENT_TARGET=${OSX} && make
+fi
+
+if [ ! -f "${CWD}/src/gperftools/.libs/libtcmalloc.a" ]; then
+    cd ${CWD}/src/gperftools
+    ./autogen.sh
+    ./configure --disable-shared
+    make -j${MKJOBS}
+fi
+
+if [ ! -f "${CWD}/src/skia/out/build/libskia.a" ]; then
+    cd ${CWD}/src/skia
+    bin/gn gen out/build --args='is_official_build=true is_debug=false extra_cflags=["-Wno-error"] skia_use_system_expat=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false skia_use_system_icu=false skia_use_system_harfbuzz=false'
+    ninja -C out/build -j${MKJOBS} skia
+fi
 
 cd ${CWD}
 if [ -d "build" ]; then
@@ -91,3 +136,4 @@ elif [ "${SNAP}" = 1 ]; then
 fi
 
 hdiutil create -volname "${VOLNAME}" -srcfolder dmg -ov -format UDBZ ${DMG}.dmg
+echo "* [Latest download for macOS ${OSX}+](https://sourceforge.net/projects/friction/files/snapshots/${YEAR}/${MONTH}/${DMG}.dmg/download)" > friction-latest-macOS${OSX}.md
