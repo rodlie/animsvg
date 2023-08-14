a146a167e5fe4702d8ceaf4ffe556cbe22565c18
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2023-08-14 18:19:09 +0200

    easing presets bugfix

--- src/scripts/easing/presets/easeInQuart.js
+++ src/scripts/easing/presets/easeInQuart.js
@@ -11,7 +11,8 @@ startVal = __START_VALUE__;
 endVal = __END_VALUE__;
 startFrame = __START_FRAME__;
 endFrame = __END_FRAME__;
-duration = endFrame-startFrame;
+duration = endFrame - startFrame;
+endVal = endVal - startVal;
 if (frame < startFrame) { frame = startFrame; }
 else if (frame > endFrame) { frame = endFrame; }
 if (frame >= startFrame && frame <= endFrame) { frame = frame - startFrame; }
