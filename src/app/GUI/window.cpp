b277c9a3ba9c84f07a6ab61905af795a6f1f80d2
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2023-09-11 21:47:43 +0200

    Added generic window class

---
+++ src/app/GUI/window.cpp
@@ -0,0 +1,97 @@
+/*
+#
+# Friction - https://friction.graphics
+#
+# Copyright (c) Friction contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# See 'README.md' for more information.
+#
+*/
+
+#include "window.h"
+#include "appsupport.h"
+
+#include <QDebug>
+#include <QVBoxLayout>
+#include <QWindow>
+
+Window::Window(QWidget *parent,
+               QWidget *child,
+               const QString title,
+               const QString id,
+               bool visible,
+               bool blockEscKey)
+    : QDialog(parent)
+    , mBlockEscKey(blockEscKey)
+{
+    setWindowFlags(Qt::Window |
+                   Qt::WindowTitleHint |
+                   Qt::WindowSystemMenuHint |
+                   Qt::WindowMinimizeButtonHint |
+                   Qt::WindowMaximizeButtonHint |
+                   Qt::WindowCloseButtonHint);
+    setWindowTitle(title);
+    setObjectName(id);
+
+    setContentsMargins(0, 0, 0, 0);
+    QVBoxLayout *mLayout = new QVBoxLayout(this);
+    mLayout->setMargin(0);
+    mLayout->addWidget(child);
+
+    loadState(visible);
+}
+
+Window::~Window()
+{
+    saveState();
+}
+
+void Window::focusWindow()
+{
+    if (!isVisible()) { show(); }
+    if (windowHandle()) { windowHandle()->requestActivate(); }
+}
+
+void Window::keyPressEvent(QKeyEvent *event)
+{
+    if (mBlockEscKey && event->key() == Qt::Key_Escape) { return; }
+    QDialog::keyPressEvent(event);
+}
+
+void Window::loadState(bool visible)
+{
+    qDebug() << "load window state" << objectName();
+    restoreGeometry(AppSupport::getSettings("ui",
+                                            QString("%1Geometry")
+                                            .arg(objectName())).toByteArray());
+    bool isMax = AppSupport::getSettings("ui",
+                                         QString("%1Maximized").arg(objectName()),
+                                         false).toBool();
+    if (!visible) { return; }
+    if (isMax) { showMaximized(); }
+    else { showNormal(); }
+}
+
+void Window::saveState()
+{
+    qDebug() << "save window state" << objectName();
+    AppSupport::setSettings("ui",
+                            QString("%1Geometry").arg(objectName()),
+                            saveGeometry());
+    AppSupport::setSettings("ui",
+                            QString("%1Maximized").arg(objectName()),
+                            isMaximized());
+}
