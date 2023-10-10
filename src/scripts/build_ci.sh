a631e448efd9b77ee65c359363d438d475a89a10
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2023-10-11 00:02:18 +0200

    Update build_ci.sh

--- src/scripts/build_ci.sh
+++ src/scripts/build_ci.sh
@@ -89,6 +89,9 @@ fi
 
 if [ ! -f "${CWD}/src/skia/out/build/libskia.a" ]; then
     cd ${CWD}/src/skia
+    if [ ! -f "bin/gn" ]; then
+        (cd bin ; python3 fetch-gn)
+    fi
     bin/gn gen out/build --args='is_official_build=true is_debug=false cc="clang" cxx="clang++" extra_cflags=["-Wno-error"] target_os="linux" target_cpu="x64" skia_use_system_expat=true skia_use_system_freetype2=true skia_use_system_libjpeg_turbo=true skia_use_system_libpng=true skia_use_system_libwebp=true skia_use_system_zlib=false skia_use_system_icu=true skia_use_system_harfbuzz=true skia_use_dng_sdk=false'
     ninja -C out/build -j${MKJOBS} skia
 fi
