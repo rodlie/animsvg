3b3dc63e8a2a9fba9d2403b0692aa0c109101a4f
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2023-10-10 23:21:12 +0200

    Update CI

--- src/scripts/build_ci.sh
+++ src/scripts/build_ci.sh
@@ -24,23 +24,7 @@ set -e -x
 
 CI=${CI:-0}
 APT=${APT:-0}
-SKIA_SYNC=${SKIA_SYNC:-1}
 PC=${PC:-""}
-
-SF_NET_SRC="https://sourceforge.net/projects/friction/files/source"
-SF_NET_SNAP="https://sourceforge.net/projects/friction/files/snapshots"
-
-SKIA_GIT="4fcb5c225a"
-GPERF_GIT="4df0b85"
-
-SKIA_TAR=${SKIA_TAR:-0}
-GPERF_TAR=${GPERF_TAR:-0}
-
-FRICTION_DIST=/snapshots/distfiles
-
-if [ "${SKIA_TAR}" = 1 ]; then
-    SKIA_SYNC=0
-fi
 
 if [ "${APT}" = 1 ]; then
 sudo apt update -y
@@ -54,7 +38,6 @@ autoconf \
 automake \
 cmake \
 python3 \
-python-is-python3 \
 ninja-build \
 tree \
 libfontconfig1-dev \
@@ -73,11 +56,19 @@ qtbase5-dev \
 qtdeclarative5-dev-tools \
 qtdeclarative5-dev \
 qtmultimedia5-dev \
-qttools5-dev-tools
+qttools5-dev-tools \
+libexpat1-dev \
+libfreetype-dev \
+libjpeg-turbo8-dev \
+libpng-dev \
+libwebp-dev \
+zlib1g-dev \
+libicu-dev \
+libharfbuzz-dev
 fi
 
 if [ "${CI}" = 1 ]; then
-    git submodule update -i --recursive
+    git submodule update -i
 fi
 
 if [ "${PC}" != "" ]; then
@@ -90,16 +81,6 @@ COMMIT=`git rev-parse --short HEAD`
 BRANCH=`git rev-parse --abbrev-ref HEAD`
 
 if [ ! -f "${CWD}/src/gperftools/.libs/libtcmalloc.a" ]; then
-    if [ "${GPERF_TAR}" = 1 ]; then
-        rm -rf ${CWD}/src/gperftools || true
-        if [ -f "${FRICTION_DIST}/gperftools.tar.xz" ]; then
-            tar xf ${FRICTION_DIST}/gperftools.tar.xz
-        else
-            curl -k -L "${SF_NET_SRC}/gperftools-${GPERF_GIT}.tar.xz/download" --output ${CWD}/gperftools.tar.xz
-            tar xf ${CWD}/gperftools.tar.xz
-        fi
-        mv gperftools-${GPERF_GIT} ${CWD}/src/gperftools
-    fi
     cd ${CWD}/src/gperftools
     ./autogen.sh
     CC=clang CXX=clang++ ./configure --disable-shared
@@ -107,21 +88,8 @@ if [ ! -f "${CWD}/src/gperftools/.libs/libtcmalloc.a" ]; then
 fi
 
 if [ ! -f "${CWD}/src/skia/out/build/libskia.a" ]; then
-    if [ "${SKIA_TAR}" = 1 ]; then
-        rm -rf ${CWD}/src/skia || true
-        if [ -f "${FRICTION_DIST}/skia.tar.xz" ]; then
-            tar xf ${FRICTION_DIST}/skia.tar.xz
-        else
-            curl -k -L "${SF_NET_SRC}/skia-${SKIA_GIT}-minimal.tar.xz/download" --output ${CWD}/skia.tar.xz
-            tar xf ${CWD}/skia.tar.xz
-        fi
-        mv skia-${SKIA_GIT} ${CWD}/src/skia
-    fi
     cd ${CWD}/src/skia
-    if [ "${SKIA_SYNC}" = 1 ]; then
-        python3 tools/git-sync-deps || true
-    fi
-    bin/gn gen out/build --args='is_official_build=true is_debug=false cc="clang" cxx="clang++" extra_cflags=["-Wno-error"] target_os="linux" target_cpu="x64" skia_use_system_expat=false skia_use_system_freetype2=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false skia_use_system_icu=false skia_use_system_harfbuzz=false skia_use_dng_sdk=false'
+    bin/gn gen out/build --args='is_official_build=true is_debug=false cc="clang" cxx="clang++" extra_cflags=["-Wno-error"] target_os="linux" target_cpu="x64" skia_use_system_expat=true skia_use_system_freetype2=true skia_use_system_libjpeg_turbo=true skia_use_system_libpng=true skia_use_system_libwebp=true skia_use_system_zlib=false skia_use_system_icu=true skia_use_system_harfbuzz=true skia_use_dng_sdk=false'
     ninja -C out/build -j${MKJOBS} skia
 fi
 
@@ -131,6 +99,7 @@ mkdir build
 cd build
 
 cmake -G Ninja \
+-DUSE_SKIA_SYSTEM_LIBS=ON \
 -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_INSTALL_PREFIX=/usr \
 -DCMAKE_CXX_COMPILER=clang++ \
