e5d7263d3db8233d08cb2c550275705204088817
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2023-12-25 14:42:34 +0100

    Bump version

--- src/core/appsupport.cpp
+++ src/core/appsupport.cpp
@@ -151,7 +151,7 @@ const QString AppSupport::getAppUrl()
 
 const QString AppSupport::getAppVersion(bool html)
 {
-    QString version = QString::fromUtf8("0.9.5"); // fallback, should not happen
+    QString version = QString::fromUtf8("0.9.6"); // fallback, should not happen
 #ifdef PROJECT_VERSION
     version = QString::fromUtf8(PROJECT_VERSION);
 #endif
