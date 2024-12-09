d3ce75dfa6c5c5e3c82b361ab85301386e119ef4
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2024-12-09 22:51:42 +0100

    Update Splash

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -123,11 +123,11 @@ int main(int argc, char *argv[])
 #endif
 
     // init splash
-    QSplashScreen splash(QPixmap(":/pixmaps/splash.png"));
+    QSplashScreen splash(QPixmap(":/icons/splash/splash-00001.png"));
     splash.show();
     splash.raise();
     splash.showMessage(QObject::tr("Loading ..."),
-                       Qt::AlignLeft | Qt::AlignBottom, Qt::white);
+                       Qt::AlignRight | Qt::AlignBottom, Qt::white);
 
     // init hardware
 #ifdef Q_OS_WIN
@@ -215,8 +215,9 @@ int main(int argc, char *argv[])
 #endif
 
     splash.raise();
+    splash.setPixmap(QPixmap(":/icons/splash/splash-00002.png"));
     splash.showMessage(QObject::tr("Initializing ..."),
-                       Qt::AlignLeft | Qt::AlignBottom, Qt::white);
+                       Qt::AlignRight | Qt::AlignBottom, Qt::white);
 
     // load settings
     try { settings.loadFromFile(); }
@@ -256,8 +257,9 @@ int main(int argc, char *argv[])
 
     // init shaders
     splash.raise();
+    splash.setPixmap(QPixmap(":/icons/splash/splash-00003.png"));
     splash.showMessage(QObject::tr("Loading Shaders ..."),
-                       Qt::AlignLeft | Qt::AlignBottom, Qt::white);
+                       Qt::AlignRight | Qt::AlignBottom, Qt::white);
     try {
         effectsLoader.iniShaderEffects();
     } catch(const std::exception& e) {
@@ -278,8 +280,9 @@ int main(int argc, char *argv[])
 
     // init audio
     splash.raise();
+    splash.setPixmap(QPixmap(":/icons/splash/splash-00004.png"));
     splash.showMessage(QObject::tr("Loading Audio ..."),
-                       Qt::AlignLeft | Qt::AlignBottom, Qt::white);
+                       Qt::AlignRight | Qt::AlignBottom, Qt::white);
     eSoundSettings soundSettings;
     AudioHandler audioHandler;
 
@@ -295,8 +298,9 @@ int main(int argc, char *argv[])
 
     // init encoder
     splash.raise();
+    splash.setPixmap(QPixmap(":/icons/splash/splash-00005.png"));
     splash.showMessage(QObject::tr("Loading Encoder ..."),
-                       Qt::AlignLeft | Qt::AlignBottom, Qt::white);
+                       Qt::AlignRight | Qt::AlignBottom, Qt::white);
     const auto videoEncoder = enve::make_shared<VideoEncoder>();
     RenderHandler renderHandler(document, audioHandler,
                                 *videoEncoder, memoryHandler);
@@ -305,8 +309,9 @@ int main(int argc, char *argv[])
     AppSupport::checkFFmpeg(isRenderer);
 
     splash.raise();
+    splash.setPixmap(QPixmap(":/icons/splash/splash-00006.png"));
     splash.showMessage(QObject::tr("Loading User Interface ..."),
-                       Qt::AlignLeft | Qt::AlignBottom, Qt::white);
+                       Qt::AlignRight | Qt::AlignBottom, Qt::white);
 
     // load UI
     const QString openProject = argc > 1 ? argv[1] : QString();
